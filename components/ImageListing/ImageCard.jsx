695b5eab21eedac55dc480eeb9ca0c505e7a81a9
Brandon Lee <brandononlyu@gmail.com>
2023-10-18 03:11:27 -0400

    Allow static image listing

--- components/ImageListing/ImageCard.jsx
+++ components/ImageListing/ImageCard.jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 class ImageCard extends React.Component {
 	render() {
 		return (
-			<div className={listStyles.card}>
+			<div className={listStyles.card + (this.props.interactable ? ` ${listStyles.interactable}` : "")}>
 				<Image src={this.props.src} width={this.props.size} height={this.props.size} objectFit="cover" alt="" />
 				<div className={listStyles.contents}>
 					<h3 className={listStyles.title}>{this.props.title}</h3>
--- pages/projects.js
+++ pages/projects.js
@@ -14,42 +14,42 @@ export default function Home() {
       <h1>Projects</h1>
       <h2>Full-fleged Applications</h2>
       <ImageListing>
-        <ImageCard src="/projects/vocaloidparser.png" size="96" title="VOCALOIDParser">
+        <ImageCard src="/projects/vocaloidparser.png" size="96" title="VOCALOIDParser" interactable>
           <Link href="https://github.com/SixBeeps/VOCALOIDParser">Visit GitHub Repo</Link><br />
           <i>Library for parsing VOCALOID projects</i>
         </ImageCard>
-        <ImageCard src="/projects/grep-health.png" size="96" title="grep-health">
+        <ImageCard src="/projects/grep-health.png" size="96" title="grep-health" interactable>
           <Link href="https://devpost.com/software/grep-health">Visit Devpost Submission</Link><br />
           <i>Digests health advice into a readable format</i>
         </ImageCard>
-        <ImageCard src="/projects/whisker.png" size="96" title="Whisker">
+        <ImageCard src="/projects/whisker.png" size="96" title="Whisker" interactable>
           <Link href="https://relay-jam-3.epicgamer007.repl.co/">Visit website</Link><br />
           <i>Simple music sequencing tool</i>
         </ImageCard>
       </ImageListing>
       <h2>Smaller Web Toys</h2>
       <ImageListing>
-        <ImageCard src="/projects/query.png" size="96" title="Query">
+        <ImageCard src="/projects/query.png" size="96" title="Query" interactable>
           <Link href="https://query.sixbeeps.com">Visit website</Link><br />
           <i>Interactive version of my song &apos;Query&apos;</i>
         </ImageCard>
-        <ImageCard src="/projects/ideas.png" size="96" title="Ideas Generator">
+        <ImageCard src="/projects/ideas.png" size="96" title="Ideas Generator" interactable>
           <Link href="https://ideas.sixbeeps.com">Visit website</Link><br />
           <i>Ideas generator that gives you words to work with</i>
         </ImageCard>
-        <ImageCard src="/projects/rtposterator.png" size="96" title="Repl Talk Posterator">
+        <ImageCard src="/projects/rtposterator.png" size="96" title="Repl Talk Posterator" interactable>
           <Link href="https://rtposterator.sixbeeps.com">Visit website</Link><br />
           <i>Markov-based model for generating fake Repl Talk posts</i>
         </ImageCard>
-        <ImageCard src="/projects/twrb.png" size="96" title="The Work-Rest Balance">
+        <ImageCard src="/projects/twrb.png" size="96" title="The Work-Rest Balance" interactable>
           <Link href="https://twrb.sixbeeps.com">Visit website</Link><br />
           <i>Small Twine story based on Space Station 13</i>
         </ImageCard>
-        <ImageCard src="/projects/cubegame.png" size="96" title="Cube Game Online">
+        <ImageCard src="/projects/cubegame.png" size="96" title="Cube Game Online" interactable>
           <Link href="https://cubegame.sixbeeps.com">Visit website</Link><br />
           <i>Really simple &quot;game&quot; using Socket.io</i>
         </ImageCard>
-        <ImageCard src="/projects/couldve.png" size="96" title="could&apos;ve">
+        <ImageCard src="/projects/couldve.png" size="96" title="could&apos;ve" interactable>
           <Link href="https://couldve.sixbeeps.com">Visit website</Link><br />
           <i>What you could&apos;ve done with your life</i>
         </ImageCard>

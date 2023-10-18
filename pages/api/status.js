f875a3fbc10b47f94ccf9de4c2e37adbddca2ada
Brandon Lee <brandononlyu@gmail.com>
2023-10-17 22:53:14 -0400

    This should be async

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,12 +1,17 @@
-export default function handler(req, res) {
-	setTimeout(
-		() => {
-			res.status(200).json({ status: {
-				ppu: 'Online',
-				eu: 'Online',
-				kdb: 'Online'
-			},
-		explanation: 'All systems go!' });
-		}, 10000 * Math.random() + 1000
-	);
+export default async function handler(req, res) {
+	return new Promise(resolve => {
+		setTimeout(
+			() => {
+				res.status(200).json({
+					status: {
+						ppu: 'Online',
+						eu: 'Online',
+						kdb: 'Online'
+					},
+					explanation: 'All systems go!'
+				});
+				resolve();
+			}, 10000 * Math.random() + 1000
+		);	
+	})
   }

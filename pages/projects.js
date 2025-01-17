aa3ea9aa5ff328eb096a73007c1d86c50618d9dd
Brandon Lee <brandononlyu@gmail.com>
2025-01-16 19:50:48 -0500

    Update projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,63 +1,74 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Header from '../components/HeaderStuff/Header.jsx'
-import ImageListing from '../components/ImageListing/ImageListing'
-import ImageCard from '../components/ImageListing/ImageCard'
+import CleanImageListing from '../components/CleanImageListing/CleanImageListing'
+import CleanImageCard from '../components/CleanImageListing/CleanImageCard'
 import Link from 'next/link'
 import Footer from '../components/FooterStuff/Footer'
+import LandingPageCard from '../components/LandingCard/LandingCard'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className='expandpage'>
       <Header />
-      <h1>Projects</h1>
-      <h2>Full-fleged Applications</h2>
-      <ImageListing>
-        <ImageCard src="/projects/vocaloidparser.png" size="128" title="VOCALOIDParser" interactable>
-          <Link href="https://github.com/SixBeeps/VOCALOIDParser">Visit GitHub Repo</Link><br />
-          <i>Library for parsing VOCALOID projects</i>
-        </ImageCard>
-        <ImageCard src="/projects/grep-health.png" size="128" title="grep-health" interactable>
-          <Link href="https://devpost.com/software/grep-health">Visit Devpost Submission</Link><br />
-          <i>Digests health advice into a readable format</i>
-        </ImageCard>
-        <ImageCard src="/projects/whisker.png" size="128" title="Whisker" interactable>
-          <Link href="https://relay-jam-3.epicgamer007.repl.co/">Visit website</Link><br />
-          <i>Simple music sequencing tool</i>
-        </ImageCard>
-        <ImageCard src="/projects/cyan.png" size="128" title="Project Cyan" interactable>
-          <Link href="https://csds-393-chisel-cyan.vercel.app">Visit website</Link><br />
-          <i>Plan courses based on schedule and reviews</i>
-        </ImageCard>
-      </ImageListing>
-      <h2>Smaller Web Toys</h2>
-      <ImageListing>
-        <ImageCard src="/projects/query.png" size="96" title="Query" interactable>
-          <Link href="https://query.sixbeeps.com">Visit website</Link><br />
-          <i>Interactive version of my song &apos;Query&apos;</i>
-        </ImageCard>
-        <ImageCard src="/projects/ideas.png" size="96" title="Ideas Generator" interactable>
-          <Link href="https://ideas.sixbeeps.com">Visit website</Link><br />
-          <i>Ideas generator that gives you words to work with</i>
-        </ImageCard>
-        <ImageCard src="/projects/rtposterator.png" size="96" title="Repl Talk Posterator" interactable>
-          <Link href="https://rtposterator.sixbeeps.com">Visit website</Link><br />
-          <i>Markov-based model for generating fake Repl Talk posts</i>
-        </ImageCard>
-        <ImageCard src="/projects/twrb.png" size="96" title="The Work-Rest Balance" interactable>
-          <Link href="https://twrb.sixbeeps.com">Visit website</Link><br />
-          <i>Small Twine story based on Space Station 13</i>
-        </ImageCard>
-        <ImageCard src="/projects/cubegame.png" size="96" title="Cube Game Online" interactable>
-          <Link href="https://cubegame.sixbeeps.com">Visit website</Link><br />
-          <i>Really simple &quot;game&quot; using Socket.io</i>
-        </ImageCard>
-        <ImageCard src="/projects/couldve.png" size="96" title="could&apos;ve" interactable>
-          <Link href="https://couldve.sixbeeps.com">Visit website</Link><br />
-          <i>What you could&apos;ve done with your life</i>
-        </ImageCard>
-      </ImageListing>
+      <LandingPageCard theme="dark">
+        <h2>My projects</h2>
+        <p>Here are some full-scale projects that I try to maintain every once in a while</p>
+        <CleanImageListing>
+            <CleanImageCard
+              src="/projects/compstar.png"
+              href="https://www.compstar.app"
+              title="compstar"
+              size={100}
+            >
+              <p>Site for hosting creative competitions</p>
+            </CleanImageCard>
+            <CleanImageCard
+              src="/projects/vocaloidparser.png"
+              href="https://github.com/SixBeeps/VOCALOIDParser"
+              title="VOCALOIDParser"
+              size={100}
+            >
+              <p>Get the <Link href="https://www.nuget.org/packages/VOCALOIDParser" target="_blank">NuGet package</Link></p>
+              <p>.NET library for working with VOCALOID5 projects</p>
+            </CleanImageCard>
+            <CleanImageCard
+              src="/projects/lqw.png"
+              href="https://discord.com/api/oauth2/authorize?client_id=1197255386470826174&permissions=2147560448&scope=applications.commands+bot"
+              title="LQWBot"
+              size={100}
+            >
+              <p>Site for understanding and learning about medical symptoms</p>
+            </CleanImageCard>
+          </CleanImageListing>
+          <br />
+          <p>and here are some projects that I've worked on in the past</p>
+          <CleanImageListing>
+            <CleanImageCard
+              src="/projects/grep-health.png"
+              href="https://devpost.com/software/grep-health"
+              title="grep-health"
+              size={100}
+            >
+              <p>Site for understanding and learning about medical symptoms</p>
+            </CleanImageCard>
+            <CleanImageCard
+              src="/projects/mixxx.png"
+              href="https://gist.github.com/SixBeeps/1632ae049c93f34ddf0feec4c673066f"
+              title="Mixxx Scrobbler"
+              size={100}
+            >
+              <p>Script for scrobbling Mixxx .cue files</p>
+            </CleanImageCard>
+            <CleanImageCard
+              src="/projects/cyan.png"
+              href="https://csds-393-chisel-cyan.vercel.app/"
+              title="Project Cyan"
+              size={100}
+            >
+              <p>Site for planning coursework based on schedule and reviews</p>
+            </CleanImageCard>
+          </CleanImageListing>
+      </LandingPageCard>
       <Footer />
     </div>
   )
